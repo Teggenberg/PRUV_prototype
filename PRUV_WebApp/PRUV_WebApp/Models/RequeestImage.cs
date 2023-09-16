5998dad9eebdabecbc8d7932b2242667b85f391a
Teggenberg <tdeggenberger@student.fullsail.edu>
2023-09-15 20:26:52 -0500

    photo upload element displays

---
+++ PRUV_WebApp/PRUV_WebApp/Models/RequeestImage.cs
@@ -0,0 +1,9 @@
+﻿namespace PRUV_WebApp.Models
+{
+    public class RequeestImage
+    {
+        public int ImageId { get; set; }
+        public int? RequestId { get; set; }
+        public byte[]? Image { get; set; }
+    }
+}
